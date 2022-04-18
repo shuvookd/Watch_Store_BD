2ed184b385b7b5ad5999f52dbc4820ba0026354d
shuvokd <shuvokd1210@gmail.com>
2022-04-18 23:36:35 +0600

    Hook complete

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import useData from '../hooks/useData';
 import HomeReview from '../Review/Review';
+import useData from './../hooks/useData';
 
 const Review = () => {
-    // const [information ]=useData([]);
+    const [information ]=useData([]);
     const [newapi,setapi]=useState('');
     const [searchtext,setsearchtext]=useState([]);
 
